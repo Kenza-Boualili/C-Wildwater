4595502c72449c91ccb79ac1d2dc8a7689a9e07a
kenzaboualili <kenza.bll78@gmail.com>
2025-12-05 19:53:46 +0100

    Create structures.h

---
+++ structures.h
@@ -0,0 +1,64 @@
+#ifndef STRUCTURES_H
+#define STRUCTURES_H
+
+//Pour les histogrammes
+
+typedef struct DonneesUsine {
+    char* identifiant;              
+    double capacite_max;         
+    double total_capte;          
+    double total_traite;         
+} DonneesUsine;
+
+typedef struct NoeudAVLUsine {
+    DonneesUsine* donnees;              
+    struct NoeudAVLUsine* gauche;       
+    struct NoeudAVLUsine* droit;        
+    int hauteur;                        
+} NoeudAVLUsine;
+
+
+//Pour l'arbre de distribution
+typedef enum {
+    NOEUD_SOURCE,
+    NOEUD_USINE,        
+    NOEUD_STOCKAGE,     
+    NOEUD_JONCTION,     
+    NOEUD_SERVICE,      
+    NOEUD_CLIENT       
+} TypeNoeud;
+
+typedef struct NoeudDistribution {
+    char* identifiant;                         
+    TypeNoeud type;                             
+                      
+    double volume_entrant; 
+    double pourcentage_fuite;      
+    double volume_fuite;   
+    double volume_sortant;                
+    
+    struct NoeudDistribution* parent;           
+    struct NoeudDistribution** enfants;         
+    int nb_enfants;                            
+    int capacite_enfants;                      
+} NoeudDistribution;
+
+//Pour retrouver les noeuds rapidemment
+
+typedef struct NoeudAVLRecherche {
+    char* identifiant;                          
+    NoeudDistribution* pointeur_noeud; 
+    struct NoeudAVLRecherche* gauche;          
+    struct NoeudAVLRecherche* droit;            
+    int hauteur;                               
+} NoeudAVLRecherche;
+
+typedef struct Troncon {
+    char* id_amont;
+    char* id_aval;
+    double pourcentage_fuite;   
+    double volume;              
+    char* usine_traitement;            
+} Troncon;
+
+#endif
