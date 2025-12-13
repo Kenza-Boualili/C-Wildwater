4bdf0c9d2d0a2598eb30fb324543a3c26cfca996
kenzaboualili <kenza.bll78@gmail.com>
2025-12-13 15:34:07 +0100

    Create liberation_memoire.h

---
+++ liberation_memoire.h
@@ -0,0 +1,9 @@
+#ifndef LIBERATION_MEMOIRE_H
+#define LIBERATION_MEMOIRE_H
+
+#include "structures.h"
+
+void libererNoeudDistribution(NoeudDistribution* noeud);
+void libererAVLRechercheComplet(NoeudAVLRecherche* racine);
+
+#endif
