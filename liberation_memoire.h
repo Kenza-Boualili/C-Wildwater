89e88b79172554d771cebb16f2ccfc72011d1c2b
kenzaboualili <kenza.bll78@gmail.com>
2025-12-13 15:39:03 +0100

    Delete liberation_memoire.h

--- liberation_memoire.h
+++
@@ -1,9 +0,0 @@
-#ifndef LIBERATION_MEMOIRE_H
-#define LIBERATION_MEMOIRE_H
-
-#include "structures.h"
-
-void libererNoeudDistribution(NoeudDistribution* noeud);
-void libererAVLRechercheComplet(NoeudAVLRecherche* racine);
-
-#endif
