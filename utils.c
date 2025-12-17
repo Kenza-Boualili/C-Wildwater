a53b0e6dac4fd3fd7a62c64d75f08db098930300
kenzaboualili <kenza.bll78@gmail.com>
2025-12-17 16:01:56 +0100

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,16 +1,17 @@
+#define _POSIX_C_SOURCE 200809L
 #include "utils.h"
 #include <stdlib.h>
 #include <string.h>
 
 int comparerChaines(char* chaine1, char* chaine2) {
     if (chaine1 == chaine2) return 0;
-    if (!chaine1) return -1;
-    if (!chaine2) return 1;
+    if (chaine1 == NULL) return -1;
+    if (chaine2 == NULL) return 1;
     return strcmp(chaine1, chaine2);
 }
 
 char* dupliquerChaine(char* source) {
-    if (!source) return NULL;
+    if (source == NULL) return NULL;
     return strdup(source);
 }
 
