56f7aaa8ce075d33ab13bbb39e91521094b86a3c
kenzaboualili <kenza.bll78@gmail.com>
2025-12-17 15:33:43 +0100

    Update utils.c

--- utils.c
+++ utils.c
@@ -1,58 +1,19 @@
 #include "utils.h"
 #include <stdlib.h>
+#include <string.h>
 
 int comparerChaines(char* chaine1, char* chaine2) {
-    if (chaine1 == NULL && chaine2 == NULL){
-        return 0;
-    }
-    if (chaine1 == NULL){
-        return -1;
-    }
-    if (chaine2 == NULL){
-        return 1;
-    }
-    int i = 0;
-    while (chaine1[i] != '\0' && chaine2[i] != '\0') {
-        if (chaine1[i] < chaine2[i]) {
-            return -1;  
-        } else if (chaine1[i] > chaine2[i]) {
-            return 1;   
-        }
-        i++; 
-    }
-    if (chaine1[i] == '\0' && chaine2[i] == '\0') {
-        return 0; 
-    } else if (chaine1[i] == '\0') {
-        return -1; 
-    } else {
-        return 1;
-    }
+    if (chaine1 == chaine2) return 0;
+    if (!chaine1) return -1;
+    if (!chaine2) return 1;
+    return strcmp(chaine1, chaine2);
 }
 
 char* dupliquerChaine(char* source) {
-    if (source == NULL) {
-        return NULL;
-    }
-    
-    int longueur = 0;
-    while (source[longueur] != '\0') {
-        longueur++;
-    }
-    char* copie = malloc((longueur + 1) * sizeof(char));
-    if (copie == NULL) {
-        return NULL;
-    }
-    for (int i = 0; i <= longueur; i++) {
-        copie[i] = source[i];
-    }    
-    return copie;
+    if (!source) return NULL;
+    return strdup(source);
 }
 
-
 int max(int a, int b) {
-    if (a > b) {
-        return a;
-    } else {
-        return b;
-    }
+    return (a > b) ? a : b;
 }
