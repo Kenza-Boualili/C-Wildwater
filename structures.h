9ec891fcda695e455260988145fecd76b6f499a3
kenzaboualili <kenza.bll78@gmail.com>
2025-12-19 16:24:58 +0100

    Update structures.h

--- structures.h
+++ structures.h
@@ -1,57 +1,62 @@
 #ifndef STRUCTURES_H
 #define STRUCTURES_H
 
-//Pour les histogrammes
+typedef struct NoeudDistribution NoeudDistribution;
 
+//Histogrammmes des usines
 typedef struct DonneesUsine {
-    char* identifiant;              
-    double capacite_max;         
-    double total_capte;          
-    double total_traite;         
+    char* identifiant;
+    double capacite_max;
+    double total_capte;
+    double total_traite;
 } DonneesUsine;
 
 typedef struct NoeudAVLUsine {
-    DonneesUsine* donnees;              
-    struct NoeudAVLUsine* gauche;       
-    struct NoeudAVLUsine* droit;        
-    int hauteur;                        
+    DonneesUsine* donnees;
+    struct NoeudAVLUsine* gauche;
+    struct NoeudAVLUsine* droit;
+    int hauteur;
 } NoeudAVLUsine;
 
-
-//Pour l'arbre de distribution
+//arbre de distribution d'eau
 typedef enum {
     NOEUD_SOURCE,
-    NOEUD_USINE,        
-    NOEUD_STOCKAGE,     
-    NOEUD_JONCTION,     
-    NOEUD_SERVICE,      
-    NOEUD_CLIENT       
+    NOEUD_USINE,
+    NOEUD_STOCKAGE,
+    NOEUD_JONCTION,
+    NOEUD_SERVICE,
+    NOEUD_CLIENT
 } TypeNoeud;
 
-typedef struct NoeudDistribution {
-    char* identifiant;                         
-    TypeNoeud type;                             
-                      
-    double volume_entrant; 
-    double pourcentage_fuite;      
-    double volume_fuite;   
-    double volume_sortant;                
-    
-    struct NoeudDistribution* parent;           
-    struct NoeudDistribution** enfants;         
-    int nb_enfants;                            
-    int capacite_enfants;                      
-} NoeudDistribution;
+//lien entre les deux noeuds
+typedef struct {
+    NoeudDistribution* enfant;
+    double pourcentage_fuite;
+} LienDistribution;
 
-//Pour retrouver les noeuds rapidemment
+//definition du noeud en entieer
+struct NoeudDistribution {
+    char* identifiant;
+    TypeNoeud type;
 
+    double volume_entrant;
+    double pourcentage_fuite;
+    double volume_fuite;
+    double volume_sortant;
+
+    struct NoeudDistribution* parent;
+    LienDistribution* enfants;
+    int nb_enfants;
+    int capacite_enfants;
+};
+
+//avl de recherche des noeuds de distribution
 typedef struct NoeudAVLRecherche {
-    char* identifiant;                          
-    NoeudDistribution* pointeur_noeud; 
-    struct NoeudAVLRecherche* gauche;          
-    struct NoeudAVLRecherche* droit;            
-    int hauteur;                               
+    char* identifiant;
+    NoeudDistribution* pointeur_noeud;
+    struct NoeudAVLRecherche* gauche;
+    struct NoeudAVLRecherche* droit;
+    int hauteur;
 } NoeudAVLRecherche;
 
 #endif
-
