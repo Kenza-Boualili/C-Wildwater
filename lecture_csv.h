f2a6b645b9478e7332f43bf1a8dd0d81d8ac48d3
kenzaboualili <kenza.bll78@gmail.com>
2025-12-19 16:19:56 +0100

    Update lecture_csv.h

--- lecture_csv.h
+++ lecture_csv.h
@@ -1,23 +1,26 @@
 #ifndef LECTURE_CSV_H
 #define LECTURE_CSV_H
-#include "structures.h"
-#include "avl_usines.h" 
+
+#include "avl_usines.h"
 #include "avl_recherche.h"
 
+// Structure pour stocker une ligne CSV 
 typedef struct {
-    char* usine_traitement;
-    char* id_amont;
-    char* id_aval;  
-    double volume;
-    double pourcentage_fuite;
+    char* usine_traitement;   // Nom de l'usine de traitement
+    char* id_amont;           // Identifiant du noeud amont
+    char* id_aval;            // Identifiant du noeud aval
+    double volume;            // Volume d'eau (k.m³/an)
+    double pourcentage_fuite; // Pourcentage de fuite (%)
 } LigneCSV;
 
-int contientMot(char* chaine, char* sous_chaine);
-TypeNoeud deduireType(char* id);
-NoeudDistribution* creerNoeudDistribution(char* id);
-void ajouterEnfant(NoeudDistribution* parent, NoeudDistribution* enfant);
+
 LigneCSV* lire_ligne_csv(char* ligne);
 void liberer_ligne_csv(LigneCSV* ligne);
-int charger_csv(char* nom_fichier, NoeudAVLUsine** avl_usines, NoeudAVLRecherche** avl_recherche);
-
-#endif
+NoeudDistribution* creerNoeudDistribution(char* id);
+void ajouterEnfant(NoeudDistribution* parent, NoeudDistribution* enfant, double pourcentage_fuite);
+NoeudDistribution* trouverRacine(NoeudAVLRecherche* avl);
+TypeNoeud deduireType(char* id);
+int contientMot(char* chaine, char* sous_chaine);
+void verifierUsinesSansCapacite(NoeudAVLUsine* racine);
+int charger_csv(FILE* fichier, NoeudAVLUsine** avl_usines, NoeudAVLRecherche** avl_recherche, char* commande);
+#endif 
